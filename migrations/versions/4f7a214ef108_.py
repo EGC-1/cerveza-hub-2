effcccf87b979c998c857c00ba2a8c221f49455e
iratxe-pm <iratxepm04@gmail.com>
2025-11-11 11:12:47 +0100

    feat: correcion imagen

---
+++ migrations/versions/4f7a214ef108_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4f7a214ef108
+Revises: 515775c66566
+Create Date: 2025-11-11 09:48:01.684681
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4f7a214ef108'
+down_revision = '515775c66566'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
