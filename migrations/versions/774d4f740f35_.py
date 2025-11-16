de447efd6dbd610ff22705424118fb2ba5f90ddb
MNB3620 <paugonpae@alum.us.es>
2025-11-16 17:28:32 +0100

    fix:"Subida fotos"

---
+++ migrations/versions/774d4f740f35_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 774d4f740f35
+Revises: 3e0b217aba43, e58be4261d2a
+Create Date: 2025-11-11 17:13:55.846278
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '774d4f740f35'
+down_revision = ('3e0b217aba43', 'e58be4261d2a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
