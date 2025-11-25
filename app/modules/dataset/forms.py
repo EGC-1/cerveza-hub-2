c43b1f08ed2d5b61f26f97059b4ecee8fc917e80
lidiafc8 <lidiaestudios14@gmail.com>
2025-11-25 20:10:18 +0100

    feat: logic added to forms.py

--- app/modules/dataset/forms.py
+++ app/modules/dataset/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import FieldList, FormField, SelectField, StringField, ValidationError, SelectMultipleField, SubmitField, TextAreaField
 from wtforms.validators import URL, DataRequired, Optional, Length
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from app.modules.dataset.models import PublicationType, DataSet, Community
+import pandas as pd
 
 class CommunityDatasetForm(FlaskForm):
 
@@ -11,6 +12,7 @@ class CommunityDatasetForm(FlaskForm):
         description="Mantén presionada Ctrl/Cmd para seleccionar múltiples datasets."
     )
     submit = SubmitField('Guardar Datasets')
+    
 class AuthorForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     affiliation = StringField("Affiliation")
@@ -53,53 +55,80 @@ class CommunityForm(FlaskForm):
         if Community.query.filter_by(name=name.data).first():
             raise ValidationError('Ya existe una comunidad con este nombre. Por favor, elige otro.')
         
-class FeatureModelForm(FlaskForm):
-    uvl_filename = StringField("UVL Filename", validators=[DataRequired()])
-    title = StringField("Title", validators=[Optional()])
-    desc = TextAreaField("Description", validators=[Optional()])
-    publication_type = SelectField(
-        "Publication type",
-        choices=[(pt.value, pt.name.replace("_", " ").title()) for pt in PublicationType],
-        validators=[Optional()],
-    )
-    publication_doi = StringField("Publication DOI", validators=[Optional(), URL()])
-    tags = StringField("Tags (separated by commas)")
-    version = StringField("UVL Version")
-    authors = FieldList(FormField(AuthorForm))
-
-    class Meta:
-        csrf = False  # disable CSRF because is subform
-
-    def get_authors(self):
-        return [author.get_author() for author in self.authors]
-
-    def get_fmmetadata(self):
-        return {
-            "uvl_filename": self.uvl_filename.data,
-            "title": self.title.data,
-            "description": self.desc.data,
-            "publication_type": self.publication_type.data,
-            "publication_doi": self.publication_doi.data,
-            "tags": self.tags.data,
-            "uvl_version": self.version.data,
-        }
-
 
 class DataSetForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     desc = TextAreaField("Description", validators=[DataRequired()])
+    
     publication_type = SelectField(
         "Publication type",
         choices=[(pt.value, pt.name.replace("_", " ").title()) for pt in PublicationType],
         validators=[DataRequired()],
     )
+    
     publication_doi = StringField("Publication DOI", validators=[Optional(), URL()])
     dataset_doi = StringField("Dataset DOI", validators=[Optional(), URL()])
     tags = StringField("Tags (separated by commas)")
-    authors = FieldList(FormField(AuthorForm))
-    feature_models = FieldList(FormField(FeatureModelForm), min_entries=1)
+    
+    authors = FieldList(FormField(AuthorForm), min_entries=1)
+    
+    csv_file = FileField(
+        'Archivo CSV del Dataset',
+        validators=[
+            FileRequired(message='¡No has seleccionado ningún archivo!'),
+            FileAllowed(['csv'], message='¡Solo se permiten archivos .csv!')
+        ]
+    )
 
     submit = SubmitField("Submit")
+
+    def validate_csv_file(self, field):
+        if not field.data:
+            return
+
+        beer_keywords = {
+            # Identificadores
+            'name', 'nombre', 'id', 'beer_name', 'cerveza', 'brand', 'marca',
+            
+            # Métricas Químicas/Físicas
+            'abv', 'alcohol', 'ibu', 'srm', 'ebc', 'og', 'fg', 'gravity', 
+            'ph', 'color', 'bitterness', 'amargor', 'clarity',
+            
+            # Ingredientes
+            'hops', 'lupulo', 'hop', 'malt', 'malta', 'grain', 'grano', 
+            'yeast', 'levadura', 'adjuncts', 'ingredients', 'ingredientes',
+            
+            # Clasificación
+            'style', 'estilo', 'category', 'categoria', 'type', 'tipo',
+            
+            # Producción/Origen
+            'brewery', 'cerveceria', 'brewed', 'location', 'city', 'country', 
+            'pais', 'region', 'state', 'batch', 'lote',
+            
+            # Cata/Reseñas
+            'aroma', 'taste', 'sabor', 'palate', 'mouthfeel', 'cuerpo', 
+            'finish', 'review', 'rating', 'score', 'puntuacion', 'notes', 'notas',
+            
+            # Envase/Comercial
+            'oz', 'ml', 'volume', 'volumen', 'size', 'price', 'precio'
+        }
+
+        try:
+            df = pd.read_csv(field.data, nrows=0)
+            
+            csv_columns = set([col.lower() for col in df.columns])
+
+            if not csv_columns.intersection(beer_keywords):
+                raise ValidationError(
+                    f"El CSV no parece ser de cervezas. No se encontraron columnas como: {', '.join(list(beer_keywords)[:5])}..."
+                )
+
+            field.data.seek(0)
+
+        except pd.errors.EmptyDataError:
+            raise ValidationError("El archivo CSV está vacío.")
+        except Exception as e:
+            raise ValidationError(f"Error al leer el CSV: {str(e)}")
 
     def get_dsmetadata(self):
 
@@ -122,6 +151,3 @@ class DataSetForm(FlaskForm):
 
     def get_authors(self):
         return [author.get_author() for author in self.authors]
-
-    def get_feature_models(self):
-        return [fm.get_feature_model() for fm in self.feature_models]
