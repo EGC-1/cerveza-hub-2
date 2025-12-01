0369a8f3ad2880ed6d5c0971f08929e17576ac9e
mery16q <marquifer@alum.us.es>
2025-12-01 17:55:33 +0100

    fix: fixing the tests of profile

--- app/modules/profile/tests/test_unit.py
+++ app/modules/profile/tests/test_unit.py
@@ -23,7 +23,6 @@ def test_client(test_client):
 
     yield test_client
 
-
 def test_edit_profile_page_get(test_client):
     """
     Tests access to the profile editing page via a GET request.
@@ -36,16 +35,15 @@ def test_edit_profile_page_get(test_client):
     if not user:
         user = User(email="user@example.com", password="test1234")
         db.session.add(user)
-        db.session.commit()
         
-        # Crear perfil asociado
+        db.session.flush()       
+        db.session.refresh(user) 
+
         profile = UserProfile(user_id=user.id, name="Name", surname="Surname")
         db.session.add(profile)
         db.session.commit()
 
     login_response = login(test_client, "user@example.com", "test1234")
     
-
     response = test_client.get("/profile/edit")
-    
     assert response.status_code == 200
