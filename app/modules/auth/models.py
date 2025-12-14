25a35adad66e2606e9c31ebeea98776565358300
Esther Rocío <estherrociovalladolid@gmail.com>
2025-12-14 22:33:46 +0100

    fix: paréntesis

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -103,7 +103,7 @@ class UserSession (db.Model):
     __tablename__ = 'user_sessions'
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    session_key = db.Column(Text), unique = True, index = True, nullable = False)
+    session_key = db.Column(Text, unique = True, index = True, nullable = False)
     ip_address = db.Column(db.String(45))
     user_agent = db.Column(db.String(255))
     login_time = db.Column(db.DateTime, nullable = False, default = lambda:datetime.now(timezone.utc))
